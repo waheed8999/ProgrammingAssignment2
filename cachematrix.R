8b518d47eef439c4aea0bf6041418a384a59ba33
Islam.Waheed <waheed8999@users.noreply.github.com>
2015-11-23 01:16:27 +0200

    adding comments to functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+# makeCacheMatrix create a list containing a function to
+# a- set the value of the matrix
+# b- get the value of the matrix
+# c- set the value of inverse of the matrix
+# d- get the value of inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -18,7 +21,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# The following function returns the inverse of the matrix. at first it checks if
+# the inverse has already been computed. If yes, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache using
+# setinverse function.
+
+# This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
